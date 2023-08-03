6af1e77fb0ed19c928fab1e9de14212f3647ff5b
JeongHun Kim <hufs0529@naver.com>
2023-08-03 17:54:06 +0900

    chat little bit

---
+++ src/main/java/penterest/spring/chat/entity/ChatMessage.java
@@ -0,0 +1,37 @@
+package penterest.spring.chat.entity;
+
+import lombok.*;
+import penterest.spring.domain.member.entity.Member;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ChatMessage {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long messageId;
+
+    @Column(nullable = false)
+    private String content;
+
+    @Column(nullable = false)
+    private LocalDateTime sendTime;
+
+    @ManyToOne
+    @JoinColumn(name = "sender_id")
+    private Member sender;
+
+    @ManyToOne
+    @JoinColumn(name = "room_id")
+    private ChatRoom chatRoom;
+
+
+
+}
